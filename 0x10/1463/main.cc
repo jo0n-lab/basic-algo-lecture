dcfc954065fdd65bde66b8e87120490e3ec55113
cuberry <joon8454@gmail.com>
2024-06-20 23:45:53 +0900

    cursor 0x10 1463

---
+++ 0x10/1463/main.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define __DEBUG__
+#define __INPUT__
+#define __PRINT__
+
+int N;
+int cache[1000001];
+
+void print_config(string s){
+	cout.width(15);
+	cout.fill('-');
+	cout<<s<<"\n";
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin>>N;
+#ifdef __INPUT__
+	print_config("input");
+	cout<<N<<"\n";
+#endif
+
+	cache[1]=0;
+	for(int i=0;i<N;i++)
+	
+
+}
+
