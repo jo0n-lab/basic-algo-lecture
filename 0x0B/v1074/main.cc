9fe07199742d0233d7de5a36eb937b4bd80103cd
cuberry <joon8454@gmail.com>
2024-07-02 08:18:44 +0900

    cursor 0x0C 1182

--- 0x0B/v1074/main.cc
+++ 0x0B/v1074/main.cc
@@ -1,8 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define __DEBUG__
+
 int N,r,c;
 int ans=0;
+
+#ifdef __DEBUG__
+queue<int> Q;
+#endif
+
 
 //func def: where located
 //input: 
@@ -11,6 +18,9 @@ void visit(int num,int row, int col){
 	if(num==-1) return;
 	int dim=1<<num;
 	int loc=(row/dim)*2 + col/dim;
+#ifdef __DEBUG__
+	Q.push(loc);
+#endif
 	ans+=dim*dim*loc;
 	visit(num-1,row%dim,col%dim);
 }
@@ -19,6 +29,13 @@ int main(){
 	cin>>N>>r>>c;
 
 	visit(N-1,r,c);
+#ifdef __DEBUG__
+	while(!Q.empty()){
+		cout<<Q.front()<<" ";
+		Q.pop();
+	}
+	cout<<"\n";
+#endif
 	cout<<ans;
 }
 
