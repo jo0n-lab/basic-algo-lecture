16a00593497271652004db28625fd331c4691860
cuberry <joon8454@gmail.com>
2024-08-11 19:37:17 +0900

    add debug level

---
+++ 0x13/1253/main.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define __INPUT__
+#define __PRINT__
+#define __DEBUG__
+
+#ifdef __DEBUG__
+#define __D1__
+#define __D2__
+#endif
+
+
--- 0x13/v12015/main.cc
+++ 0x13/v12015/main.cc
@@ -1,13 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// #define __INPUT__
-// #define __PRINT__
-// #define __DEBUG__
+#define __INPUT__
+#define __PRINT__
+#define __DEBUG__
+
+#ifdef __DEBUG__
+#define __D1__
+#define __D2__
+#endif
 
 int N;
 int arr[1000002];
 vector<int> v;
+
+void print_config(string s){
+	cout<<__FILE__<<"::"<<s<<": ";
+}
+
+
 
 int lower_idx(int target) {
     int st = 0;
@@ -18,7 +29,8 @@ int lower_idx(int target) {
             en = mid;
         else
             st = mid + 1;
-#ifdef __DEBUG__
+#ifdef __D2__
+		print_config(__func__);
         cout << "st & en " << st << " " << en << "\n";
 #endif
     }
@@ -42,7 +54,7 @@ int main() {
 
     v.push_back(arr[1]);
     for (int i = 2; i <= N; i++) {
-#ifdef __DEBUG__
+#ifdef __D1__
         cout << "DEBUG\n";
 #endif
         int cur = arr[i];
@@ -50,12 +62,13 @@ int main() {
             v.push_back(cur);
         else if (cur < v.back()) {
             int idx = lower_idx(cur);
-#ifdef __DEBUG__
+#ifdef __D1__
+			print_config(__func__);
             cout << "cur & idx: " << cur << " " << idx << "\n";
 #endif
             v[lower_idx(cur)] = cur;
         }
-#ifdef __DEBUG__
+#ifdef __D1__
         for (int i = 0; i < v.size(); i++)
             cout << v[i] << " ";
         cout << "\n";
