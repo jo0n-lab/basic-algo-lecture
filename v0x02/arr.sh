317d9563345ee12ba7f98d030d5de82473d44e8a
cuberry <joon8454@gmail.com>
2024-08-15 15:40:29 +0900

    format 0x02 *

--- v0x02/arr.sh
+++
@@ -1,5 +0,0 @@
-for file in solutions/*.cpp; do
-  dir="solutions/${file%%.*}" # 파일 이름에서 확장자를 제외한 부분을 추출하여 폴더 이름으로 사용
-  mkdir -p "$dir" # 동일한 이름의 폴더 생성 (-p 옵션은 폴더가 이미 존재하는 경우 오류를 방지)
-  mv "$file" "$dir" # 파일을 해당 폴더로 이동
-done
--- v0x02/prs/ref.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-reference(){
-    echo "enter problem No. :"
-    read no
-    python -mwebbrowser https://www.acmicpc.net/problem/$no
-}
-
-reference
